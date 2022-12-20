d3813c1aeb8b10f4fcceb97276e6d6aaf5665dc3
Jiahua Li <59403109+jiahualihuanahuan@users.noreply.github.com>
2022-12-19 23:53:49 -0500

    Update download images from websites.py

--- download images from websites.py
+++ download images from websites.py
@@ -2,18 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 
 for id in range(523):
-    response = requests.get(f'http://www.sexinsex.net/bbs/forum-252-{id}.html')
+    response = requests.get(f'http://www.domain.net/bbs/forum-252-{id}.html')
     print(f"we are at page {id}")
     # Parse the HTML content of the page
     soup = BeautifulSoup(response.content, 'html.parser')
 
     # Find all the images on the page
-    tags = soup.find_all('th', class_='lock')
+    tags = soup.find_all('th', class_='lock/common/new')
 
     for tag in tags:
         tag.find_all('a')
         link = tag.find('span').find('a')['href']
-        response = requests.get(f'http://www.sexinsex.net/bbs/{link}')
+        response = requests.get(f'http://www.domain.net/bbs/{link}')
 
         # Parse the HTML content of the page
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -26,7 +26,7 @@ for id in range(523):
             try:
                 image_url = image['src']
                 response = requests.get(image_url)
-                open(f"D:/Media/nosql/photo/other/{image_url.split('/')[-1]}", 'wb').write(response.content)
+                open(f"D:/Media/photo/{image_url.split('/')[-1]}", 'wb').write(response.content)
                 print(f"downloaded {image_url.split('/')[-1]}")
             except:
                 print("error")
