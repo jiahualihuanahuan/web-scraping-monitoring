7e9fbe014dbc8c0e3a196d49ce967ce40635d3e7
Jiahua Li <59403109+jiahualihuanahuan@users.noreply.github.com>
2022-12-30 15:25:32 -0500

    Update sexinsex2.py

--- scrape images/sexinsex2.py
+++ scrape images/sexinsex2.py
@@ -1,52 +1,68 @@
+# this code download images from sexinsex websites
+# two loops going through different image categories and different pages under each category
+# only go to the "版块主题" section to avoid downloading unnessassary contents
+# get exact page title to properly store images into different folders
+# only download ".jpg" format to avoid downloading unnessassary contents
+
 import requests
 from bs4 import BeautifulSoup
 import os
 import time
 
 while True:
-	for id in [186,253,64,68]:
-		# print current time on each iteration
-		current_time = time.localtime()
-		current_time = time.strftime("%H:%M:%S", current_time)
-		print(f"start time is: {current_time} \n working on {id} job")
+    for page in range(1,500):
+        
+        for i in [186,253,64,68]:    
+		# 186 东方唯美图坊
+    		# 253 西方唯美图坊
+    		# 64 东方靓女集中营
+    		# 68 西洋靓女骚妹
+            # print current time on each iteration
+            current_time = time.localtime()
+            current_time = time.strftime("%H:%M:%S", current_time)
+            print(f"start time is: {current_time} \n working on id={i}, page={page}")
+            response = requests.get(f'http://www.sexinsex.net/bbs/forum-{i}-{page}.html')
+            # Parse the HTML content of the page
+            soup = BeautifulSoup(response.content, 'html.parser')
 
-		response = requests.get(f'http://www.sexinsex.net/bbs/forum-{id}-1.html')
-		# Parse the HTML content of the page
-		soup = BeautifulSoup(response.content, 'html.parser')
+            section = soup.find_all('table', summary=f"forum_{i}")[-1]
 
-		for css_class in ["new", "hot"]: # "common", "lock", "hot"
-			# Find all the images on the page; change "class_=??" to "common", "new", "lock", hot" for various results
-			tags = soup.find_all('th', class_=css_class)
+            for css_class in ["new", "hot", "common"]: # "common", "lock", "hot"
+        	# Find all the images on the page; change "class_=??" to "common", "new", "lock", hot" for various results
+                tags = section.find_all('th', class_=css_class)
 
-			for tag in tags:
-				tag.find_all('a')
-				link = tag.find('span').find('a')['href']
-				response = requests.get(f'http://www.sexinsex.net/bbs/{link}')
+                for tag in tags:
+                    tag.find_all('a')
+                    link = tag.find('span').find('a')['href']
+                    response = requests.get(f'http://www.sexinsex.net/bbs/{link}')
 
-		        # Parse the HTML content of the page
-				soup = BeautifulSoup(response.content, 'html.parser')
+                    # Parse the HTML content of the page
+                    soup = BeautifulSoup(response.content, 'html.parser')
 
-				title = soup.find('title').string[:10]
-				print(title)
+                    title = soup.find('title').string.split('-')[0]
+                    print(title)
 
-		        # Find all the images on the page
-				images = soup.find_all('img')
+                    # Find all the images on the page
+                    images = soup.find_all('img')
 
-				dir_name = f"D:/Media/nosql/photos/{title}/"
-				if not os.path.exists(dir_name):
-					os.makedirs(dir_name)
+                    dir_name = f"D:/Media/nosql/sexinsex/{title}/"
+                    if not os.path.exists(dir_name):
+                        os.makedirs(dir_name)
 
-				# Iterate over the images and download each one
-				for image in images:
-					try:
-						image_url = image['src']
-						response = requests.get(image_url)
-						print(f"downloading {image_url.split('/')[-1]}")
+                    # Iterate over the images and download each one
+                    for image in images:
+                        try:
+                            image_url = image['src']
+                            if image_url.split(".")[-1] == "jpg":
+                                response = requests.get(image_url)
+                                print(f"downloading {image_url.split('/')[-1]}")
 
-						open(f"D:/Media/nosql/photos/{title}/{image_url.split('/')[-1]}", 'wb').write(response.content)
-						print(f"downloaded {image_url.split('/')[-1]}")
-					except:
-						print("error")
+                                open(f"D:/Media/nosql/sexinsex/{title}/{image_url.split('/')[-1]}", 'wb').write(response.content)
+                                print(f"downloaded {image_url.split('/')[-1]}")
+                            else:
+                                pass
+                        except:
+                            print("error")
 	
 	# print current time before sleep
 	current_time = time.localtime()
